cc704c1a0d47df53ab9ba7968b9627f1d63e8f45
Gennaro Rende <44288923+jackrende@users.noreply.github.com>
2019-11-12 18:12:48 +0100

    Working with the shapefiles

---
+++ Laboratory 1/Step 2/map.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import shapefile as shp
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style="whitegrid", palette="pastel", color_codes=True)
+sns.mpl.rc("figure", figsize=(10,6))
+
+#opening the vector map
+shp_path = "./NILZone_U00-32.shp"
+#reading the shape file by using reader function of the shape lib
+sf = shp.Reader(shp_path)
+
+len(sf.shapes())
+sf.records()
